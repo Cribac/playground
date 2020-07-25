66c69d9764b726cbac8b622be621e650ea944874
Christian Bachhelm <cbachhelm@gmail.com>
2020-07-25 12:31:11 +0200

    Initial commit for thera2stain deno implementation.

---
+++ thera2stain_deno/README.md
@@ -0,0 +1,1 @@
+Quick'n Dirty [Deno](https://deno.land/) implementation for the [thera2stain](https://github.com/Cribac/playground/tree/master/thera2stain) script.
---
+++ thera2stain_deno/index.ts
@@ -0,0 +1,62 @@
+// @TODO
+// use proper types
+// error handling
+// get this darn mailer to work
+
+import { Cron } from 'https://deno.land/x/cron/cron.ts';
+import { SmtpClient } from "https://deno.land/x/smtp/mod.ts";
+
+const URL = 'https://www.eve-scout.com/api/wormholes?systemSearch=Jita&limit=1000&offset=0&order=asc';
+const cron = new Cron();
+
+console.log('thera2stain connection search running, hit ctrl + c to stop...');
+
+cron.add('* * * * *', () => {
+  const scoutData = fetchData(URL)
+    .then((data) => {
+      const filtered = filterforStain(data);
+
+      if (filtered.length > 0) {
+
+        let mailBody = '';
+        filtered.forEach((signal: any) => {
+          mailBody += `System: ${signal.solarSystemName} with signature: ${signal.signatureId} \n`;
+        });
+
+        console.log('Mailing results')
+        // connect to smtp client with default mailcatcher credentials.
+        const client = new SmtpClient();
+        // @ts-ignore
+        client.connect({
+          hostname: '127.0.0.1',
+          port: 1025,
+        });
+
+        client.send({
+          from: "mailaddress@163.com",
+          to: 'to-address@xx.com',
+          subject: 'Thera connection to Stain detected',
+          content: mailBody,
+        });
+
+        client.close();
+
+      } else {
+        console.log('Nothing so far...');
+      }
+    });
+});
+
+function fetchData(url: string): Promise<{}> {
+  return fetch(url).then((response) => response.json());
+}
+
+function filterforStain(data: any) {
+  return data.filter((elem: any) => elem.destinationSolarSystem.region.name === 'Stain');
+}
+
+console.table(cron.cronJobs);
+
+cron.start();
+
+export { };
