58f7d67c93e2f6ac0d3ddf5fb71260fe63f7ea03
Christian Bachhelm <cbachhelm@gmail.com>
2020-07-25 12:33:58 +0200

    Clean up imports

--- thera2stain_deno/index.ts
+++ thera2stain_deno/index.ts
@@ -4,14 +4,14 @@
 // get this darn mailer to work
 
 import { Cron } from 'https://deno.land/x/cron/cron.ts';
-import { SmtpClient } from "https://deno.land/x/smtp/mod.ts";
+import { SmtpClient } from 'https://deno.land/x/smtp/mod.ts';
 
 const URL = 'https://www.eve-scout.com/api/wormholes?systemSearch=Jita&limit=1000&offset=0&order=asc';
 const cron = new Cron();
 
 console.log('thera2stain connection search running, hit ctrl + c to stop...');
 
-cron.add('* * * * *', () => {
+cron.add('0 */4 * * *', () => {
   const scoutData = fetchData(URL)
     .then((data) => {
       const filtered = filterforStain(data);
@@ -26,14 +26,13 @@ cron.add('* * * * *', () => {
         console.log('Mailing results')
         // connect to smtp client with default mailcatcher credentials.
         const client = new SmtpClient();
-        // @ts-ignore
         client.connect({
           hostname: '127.0.0.1',
           port: 1025,
         });
 
         client.send({
-          from: "mailaddress@163.com",
+          from: 'thera2stain@example.com',
           to: 'to-address@xx.com',
           subject: 'Thera connection to Stain detected',
           content: mailBody,
