8e469ceb7fe45acde8098d81fe2ca03deb31a5d6
Christian Bachhelm <cbachhelm@tuta.io>
2025-01-01 17:23:56 +0100

    [vite-react] Rename Hello to CursorContainer

--- vite-react/src/App.tsx
+++ vite-react/src/App.tsx
@@ -1,10 +1,10 @@
 import './global.css'
-import Hello from './components/CursorContainer'
+import CursorContainer from './components/CursorContainer'
 
 function App() {
   return (
     <main className='bg-slate-500 h-screen flex justify-center items-center text-base text-white'>
-      <Hello />
+      <CursorContainer />
     </main>
   )
 }
