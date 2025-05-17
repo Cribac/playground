aced2d6e6a47c50162516b243452102d28e7b82f
Christian Bachhelm <cbachhelm@tuta.io>
2025-05-17 11:23:19 +0200

    Revisit .gitignore

--- vite-react/src/App.tsx
+++ vite-react/src/App.tsx
@@ -1,10 +1,29 @@
 import './global.css'
-import CursorContainer from './components/CursorContainer'
+import glaze from 'glazejs'
+import gsap from 'gsap';
+//import CursorContainer from './components/CursorContainer'
+
+console.log('fuuuuuuuuuu', gsap)
+
+glaze({
+  lib: {
+    gsap: {
+      core: gsap,
+    },
+  },
+})
 
 function App() {
   return (
     <main className='bg-slate-500 h-screen flex justify-center items-center text-base text-white'>
-      <CursorContainer />
+      <h1 data-animate="@lg:[&>span>span]:to:y-0|stagger-0.025" className="text-black">
+        {"Utility-based animations for the web.".split(" ").map((w, i, arr) => (
+          <span className="relative inline-block overflow-hidden" key={i}>
+            <span className="inline-block translate-y-full">{w}</span>
+          </span>
+        ))}
+      </h1>
+      {/* <CursorContainer /> */}
     </main>
   )
 }
