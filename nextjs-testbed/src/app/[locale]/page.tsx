daa0a79db4c6a316498d3eb1a90d53c0d06b53fb
Christian Bachhelm <cbachhelm@tuta.io>
2025-05-20 20:20:49 +0200

    [nextjs-testbed] Add a locale switcher component

--- nextjs-testbed/src/app/[locale]/page.tsx
+++ nextjs-testbed/src/app/[locale]/page.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import { Locale, useTranslations } from 'next-intl'
 import { setRequestLocale } from 'next-intl/server'
 import { use } from 'react'
+import LocaleSwitcher from '@/app/components/ui/LocaleSwitcher'
 
 type Props = {
   params: Promise<{ locale: Locale }>
@@ -11,10 +12,11 @@ export default function Home({ params }: Props) {
   const { locale } = use(params)
   setRequestLocale(locale)
   const t = useTranslations('HomePage')
-  
+
   return (
     <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
       <main className='flex flex-col gap-[32px] row-start-2 items-center sm:items-start'>
+        <LocaleSwitcher />
         <Image
           className='dark:invert'
           src='/next.svg'
